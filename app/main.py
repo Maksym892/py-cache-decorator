bf27320a0438ff4c232de0a18b016c781ab2eb53
Maksym Zhmura <zmuramaksim@gmail.com>
2026-01-17 22:11:54 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
+import functools
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
     # Write your code here
-    pass
+    cashe_dict = {}
+    functools.wraps(func)
+
+    def wrapper(*args, **kwargs) -> dict:
+        keyword = ""
+        for arg in args:
+            keyword += str(arg)
+        if keyword in cashe_dict.keys():
+            print("Getting from cache ")
+        else:
+            print("Calculating new result")
+            cashe_dict[keyword] = func(*args, **kwargs)
+        return cashe_dict[keyword]
+    return wrapper
